50f8313be1930fd8643043e9534fe534d5473507
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-02-14 21:49:12 -0300

    Terceiro commit dia 14/02/23

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,15 @@
 import User from "../models/User";
+
+
 class UsersControlller {
     async index(req, res) {
-
+        try {
+            const users = await User.finf();
+            return res.json(users);
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error. "})
+        }
     }
 
     async show(req, res) {
@@ -9,7 +17,24 @@ class UsersControlller {
     }
 
     async create(req, res) {
+        try {
+             const { email, password } = req.body;
 
+             const user = await User.findOne({ email });
+
+             if (user) {
+                return res
+                    .status(422)
+                    .json({ message:`User ${email} already exists.` });
+             }
+
+             const newUser = await User.create({ email, password });
+
+             return res.status(201).json(newUser); 
+        } catch (err) {
+             console.error(err);
+             return res.status(500).json({ error: "Internal server error." })
+        }
     }
 
     async update(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,16 @@ import { Router } from "express";
 import { route } from "express/lib/router";
 
 import HelloController from "./controllers/HelloController";
+import UsersController from "./controllers/UsersController";
 
 const routes = new Router();
 
 routes.get('/hello', HelloController.index);
+
+
+// REST
+routes.get('/users', UsersController.index);
+routes.get('/users/:id', UsersController.show);
 
 export default routes;
 
